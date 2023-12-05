5deabb05765607d3adb5c6e6b6c8292ae6069643
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-12-05 23:58:27 +0900

    入力値を受け取る

--- src/components/InputTask.tsx
+++ src/components/InputTask.tsx
@@ -1,5 +1,7 @@
 import {useRecoilState } from "recoil";
 import {inputTitleState} from "../states/inputTitleState.ts";
+import react from "@vitejs/plugin-react";
+import React from "react";
 
 export const InputTask = () => {
 
@@ -9,9 +11,13 @@ export const InputTask = () => {
             console.log(inputTitle);
         }
 
+        const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+            console.log(e);
+        }
+
     return (
         <div>
-            <input type="text" className="inputTitle" />
+            <input type="text" className="inputTitle" onChange={onChange}/>
             <button type="button" className="addName" onClick={handleClick}>追加</button>
         </div>
     )
